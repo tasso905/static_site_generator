7ad40c01a2777fb2c87c440bc87b00caa8ebe6df
tasso905 <tasso75@hotmail.com>
2025-01-31 21:50:53 -0500

    added text to textnode function.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,15 @@
 from textnode import *
+from split_nodes import *
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
