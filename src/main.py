c6a1774453be0d8994fe5ef07b8e46d622bb47d0
tasso905 <tasso75@hotmail.com>
2025-02-05 11:09:33 -0500

    Added copy recursive function to main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,38 @@
 from textnode import TextNode, TextType
 from html_node import HTMLNode, LeafNode, ParentNode
 from split_nodes import *
+import os
+import shutil
+
+def copy_recursive(src_dir, dest_dir):
+
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+        os.mkdir(dest_dir)
+
+    for item in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+            print(f"Copied file: {dest_path}")
+        elif os.path.isdir(src_path):
+            os.mkdir(dest_path)
+            print(f"Created directory: {dest_path}")
+            # Recurse into the subdirectory
+            copy_recursive(src_path, dest_path)
 
 def main():
+
+    # Define source and destination directory paths
+    src_dir = "static"
+    dest_dir = "public"
+
+    # Call your recursive function to copy contents
+    copy_recursive(src_dir, dest_dir)
+
+
     text_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(text_node)
     html_node  = HTMLNode(tag="p", value="Hello", props={"class": "greeting"})
